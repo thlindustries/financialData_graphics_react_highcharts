d502e9b50abc1b87670bdb5e184b0361c3afbf43
thlindustries <thlindustries23@gmail.com>
2020-01-15 19:44:07 -0300

    Organizando componentes

--- client/softexpert/src/components/graficos/graph1/index.js
+++ client/softexpert/src/components/graficos/graph1/index.js
@@ -1,18 +1,16 @@
 
 // //API ---->   https://financialmodelingprep.com/api/v3/financials/income-statement/{{SIMBOLO DA EMPRESA}}
 //-----------------------------------------------------------------------------------------------------
-
 import React, { Component } from 'react';
 import Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
 import Grid from '@material-ui/core/Grid';
 
-//API
+//Comunicação com a API
 import axios from 'axios';
 
-
-// import { Container } from './styles';
-const data_test=[];
+//variaveis para manipular o grafico
+const data_API=[];
 let page_init = 0;
 let lista_receita=[]
 let lista_cresciment_receita=[]
@@ -21,10 +19,12 @@ let lista_margem_ebitda=[]
 let lista_ebitda=[]
 let lista_renda_consolidada=[]
 
+//função para criar o array object (data_API) com as informações da API
 function createData(revenue, revenue_growth, operational_expenses, ebitda_margin, ebitda, consolidated_income) {
   return { revenue, revenue_growth, operational_expenses, ebitda_margin, ebitda, consolidated_income };
 }
 
+//função que remove os espaços das Keys do JSON retornado pela API
 function replaceKeys(object) {
   Object.keys(object).forEach(function (key) {
       var newKey = key.replace(/\s+/g, '');
@@ -41,11 +41,9 @@ function replaceKeys(object) {
 export default class graph1 extends Component {
   constructor(props) {
     super(props);
-
     this.state={
       dados_empresa:[]
     }
-
     axios.get('https://financialmodelingprep.com/api/v3/financials/income-statement/AAPL').then(resultado=>{
       this.setState({
         dados_empresa:resultado.data,
@@ -78,23 +76,24 @@ export default class graph1 extends Component {
       })
     })
   }
+
   render() {
     let data=this.state.dados_empresa.financials;
     if(data!==undefined && page_init===0){
+
       replaceKeys(data)
       data.map(function(item,i){
-        data_test.push(createData(data[i].Revenue,data[i].RevenueGrowth,data[i].OperatingExpenses,data[i].EBITDAMargin,data[i].EBITDA,data[i].ConsolidatedIncome))
+        data_API.push(createData(data[i].Revenue,data[i].RevenueGrowth,data[i].OperatingExpenses,data[i].EBITDAMargin,data[i].EBITDA,data[i].ConsolidatedIncome))
       })
       page_init++;
 
-      
-      data_test.map(function(item,i){
-        lista_receita.push(parseFloat(data_test[i].revenue))
-        lista_cresciment_receita.push(parseFloat(data_test[i].revenue_growth))
-        lista_despesas_operacionais.push(parseFloat(data_test[i].operational_expenses))
-        lista_margem_ebitda.push(parseFloat(data_test[i].ebitda_margin))
-        lista_ebitda.push(parseFloat(data_test[i].ebitda))
-        lista_renda_consolidada.push(parseFloat(data_test[i].consolidated_income))
+      data_API.map(function(item,i){
+        lista_receita.push(parseFloat(data_API[i].revenue))
+        lista_cresciment_receita.push(parseFloat(data_API[i].revenue_growth))
+        lista_despesas_operacionais.push(parseFloat(data_API[i].operational_expenses))
+        lista_margem_ebitda.push(parseFloat(data_API[i].ebitda_margin))
+        lista_ebitda.push(parseFloat(data_API[i].ebitda))
+        lista_renda_consolidada.push(parseFloat(data_API[i].consolidated_income))
       })
     }
     return (
@@ -115,26 +114,26 @@ export default class graph1 extends Component {
                 },
                 responsive: {
                   rules: [{
-                      condition: {
-                          maxWidth: 500
-                      },
-                      chartOptions: {
-                          legend: {
-                              layout: 'horizontal',
-                              align: 'center',
-                              verticalAlign: 'bottom'
-                          }
+                    condition: {
+                      maxWidth: 500
+                    },
+                    chartOptions: {
+                      legend: {
+                        layout: 'horizontal',
+                        align: 'center',
+                        verticalAlign: 'bottom'
                       }
+                    }
                   }]
                 },
                 yAxis: {
                   title: {
-                      text: 'Valores'
+                    text: 'Valores'
                   }
                 },
                 xAxis: {
                   accessibility: {
-                      rangeDescription: [2010,2011,2012]
+                    rangeDescription: [2010,2011,2012]
                   }
                 }
               }}
